c0df21de2042221e0cd2eb84e66463f94cf9587a
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2024-12-18 00:33:15 +0000

    ToDoFech

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValor, setInputValor] = useState("");
+	// const [listado, setListado] = useState([]);
+	const [job,setJob] = useState([]);
+	useEffect (()=>{
+		const getHacerTareas = async () => {
+			let response = await fetch("https://dummyjson.com/todos");
+			if(!response.ok){ new Error (" No se recibieron datos")}
+			let data = await response.json();
+			let newArray = data.todos.map((tarea)=> tarea.todo);
+			setJob(newArray);
+		}
+		getHacerTareas()
+	},[])
+	const listJob = job.map((t) => <li>{t}</li>);
+	
+	
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container justify-content-center">
+			
+			<h1>Mi listado de tareas</h1>
+			<div className="container d-flex justify-content-center">
+			<button type="button" class="btn btn-danger" onClick={()=> setJob(job.filter((t,currentIndex)=> job.length === currentIndex))}>Vaciar lista</button>	
+			</div>
+			
+			{listJob.length===0 ? (
+				<div>No hay tareas disponibles</div>):(
+				<ul>
+				<li>
+					<input
+						type="text" 
+						onChange={(e) => setInputValor(e.target.value)} 
+						value={inputValor}
+						onKeyUp={(e) => {
+							if (e.key === "Enter" && inputValor !=="") {								
+								setJob([...job ,inputValor]);
+								setInputValor("");
+								alert("Tarea añadida");
+							}
+						}}
+						placeholder="Introduce la tarea en el listado"></input>
+						</li>
+			
+
+				{job.map((item,index) => {
+					return(
+					<li key={index} className="d-flex justify-content-between">
+						{item}{""} <i className="fa-solid fa-trash" onClick={()=> setJob(job.filter((t,currentIndex)=> index != currentIndex))} ></i>
+						
+						</li>)}
+				)}
+
+			
+			</ul>)}
+			
+			
 		</div>
 	);
 };
